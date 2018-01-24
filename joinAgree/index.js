cf17ef7c0fd91c6941e22c41c1845040cec54f07
byuneunjin <birdiechance10@gmail.com>
2018-01-24 13:09:40 +0900

    jquery_파일수정/join&naverLogin_완료/ 로그인 백그라운드 수정

--- joinAgree/index.js
+++ joinAgree/index.js
@@ -1,15 +1,93 @@
 $(function () {
 
 
+    //약관동의
+    function customer() {
+        if ($('.costomerAgreeArea path').attr('fill') == '#77a2db') {
 
-    $(this).click(function(){
-        console.log('눌림');
-        console.log($(this).attr('type'));
-         $(this).parent().parent().next().slideToggle();
-        
+            $('.costomerAgreeArea path').attr('fill', '#909090');
+        } else {
+
+            $('.costomerAgreeArea path').attr('fill', '#77a2db');
+
+        }
+    }
+
+    $('.costomerAgreeArea path').click(function () {
+        customer();
+
     });
-    
-    
-    
-    
+
+    $('.costomerAgreeArea button').click(function () {
+
+        $('.costomerAgreeText').slideToggle();
+
+    });
+
+
+
+    //개인정보취급방침   
+
+    function infoAgree() {
+        if ($('.infoAgreeAgree path').attr('fill') == '#77a2db') {
+
+            $('.infoAgreeAgree path').attr('fill', '#909090');
+        } else {
+
+            $('.infoAgreeAgree path').attr('fill', '#77a2db');
+        }
+    }
+
+    $('.infoAgreeAgree path').click(function () {
+        infoAgree();
+    });
+
+    $('.infoAgreeAgree button').click(function () {
+
+        $('.infoAgreeAgreeText').slideToggle();
+
+    });
+
+
+
+
+    //마케팅수신동의
+
+    function marketing() {
+        if ($('.marketingAgree path').attr('fill') == '#77a2db') {
+
+            $('.marketingAgree path').attr('fill', '#909090');
+        } else {
+
+            $('.marketingAgree path').attr('fill', '#77a2db');
+
+
+
+        }
+    }
+    $('.marketingAgree path').click(function () {
+        marketing();
+    });
+
+
+
+    $('.marketingAgree button').click(function () {
+
+        $('.marketingAgreeText').slideToggle();
+
+    });
+
+
+
+
+    //전체동의하기
+    $('.totalAgreeArea path').click(function () {
+        customer();
+        infoAgree();
+        marketing();
+
+
+    });
+
+
 });
--- js/index.js
+++ js/index.js
@@ -1,22 +1,7 @@
 $(function () {
 
 
-
-    function switchBG() {
-        
-        if ($('.changeImg li').eq(0).css("display") == 'block'){
-            
-             $('.changeImg li').eq(0).fadeOut(4000);
-            
-             $('.changeImg li').eq(1).fadeIn(4000);
-            
-        }else{
-             $('.changeImg li').eq(1).fadeOut(4000);
-             $('.changeImg li').eq(0).fadeIn(4000);
-            
-        }
-    }
-    
+   // 비회원로그인/아이디패스워드 찾기 전환
     $('.nomemb button').click(function(){
         
         
@@ -53,13 +38,8 @@ $(function () {
     });
     
     
-
-    setInterval(function () {
-
-        switchBG();
-
-    }, 500);
     
+    //이메일 셀렉트 함수 
     $('#selectEmailID').change(function(){ $("#selectEmailID option:selected").each(function () { 
         if($(this).val()== '1'){ //직접입력일 경우
             $("#str_email02ID").val(''); //값 초기화
@@ -83,6 +63,41 @@ $(function () {
             } 
             }); 
                                        });
+    
+ //백그라운드 무한루프 효과
+    function switchBG() {
+        
+        if ($('.changeImg li').eq(0).css("display") == 'block'){
+            
+             $('.changeImg li').eq(0).fadeOut(4000);
+             
+            $('.changeImg li').eq(0).children().animate({'width':'1000px','height':'auto'},4000);
+            
+            
+             $('.changeImg li').eq(1).fadeIn(4000);
+            
+            $('.changeImg li').eq(0).children().animate({'width':'1500px','height':'auto'},4000);
+            
+            
+            
+        }else{
+             $('.changeImg li').eq(1).fadeOut(4000);
+            
+            $('.changeImg li').eq(1).children().animate({'width':'1000px','height':'auto'},4000);
+            
+            
+             $('.changeImg li').eq(0).fadeIn(4000);
+            
+             $('.changeImg li').eq(1).children().animate({'width':'1500px','height':'auto'},4000);
+        }
+    }
+    
+    setInterval(function () {
+
+        switchBG();
+
+    }, 500);
+    
 
     
     
